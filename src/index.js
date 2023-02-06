ea99e46d13512acb2e68d9c4bfa1a494a3fc9de6
RustamJs97 <rustamlinkin@gmail.com>
2023-02-06 23:19:40 +0500

    first commit

--- src/index.js
+++ src/index.js
@@ -11,12 +11,12 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
   <React.StrictMode>
-    <BrowserRouter>
+    `    <BrowserRouter>
       <False>
         <Sidebar />
         <Rout />
       </False>
-    </BrowserRouter>
+    </BrowserRouter>`
     {/* <Test /> */}
   </React.StrictMode>
 );
